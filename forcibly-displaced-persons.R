40b075f977d95e90fcceb3f8caef9df1cb028894
Solomon Ayuba <solomon.ayuba@miva.edu.ng>
2024-08-23 02:51:56 +0100

    project initiation

---
+++ forcibly-displaced-persons.R
@@ -0,0 +1,11 @@
+# Initiated Fri 23 August, 2024
+# data from UNHCR on forcibly displaced persons
+
+# importing custom rmd templates from 'rticles' from CRAN
+install.packages('rticles')
+library(rticles)
+
+# more rmd templates from rticles GitHub - development version
+install.packages("pak")
+pak::pak("rstudio/rticles")
+library(pak)
